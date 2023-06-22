42b38ee324f5a1a0ac600ea77b9050e167e5f6a7
Osver Fernandez Valverde <osver@live.com>
2023-06-21 18:44:20 -0600

    forgot to call functions

--- cloudflare.php
+++ cloudflare.php
@@ -2,6 +2,8 @@
 
 // Include the Mobile_Detect.php file
 require_once $_SERVER['DOCUMENT_ROOT'].'/mobileDetect/src/MobileDetect.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/functions/globalFunctions.php';
+
 
 $detect = new \Detection\MobileDetect;
 $deviceType = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
