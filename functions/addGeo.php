83ff6f613190e6c42e029a41391c287151c5abab
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-06-22 06:33:10 -0600

    added state and counrty

--- functions/addGeo.php
+++ functions/addGeo.php
@@ -6,17 +6,11 @@
   </head>
   <body>
     <form>
-        <label for="firstname">First name: </label>
-        <input type="text" name="firstname"  required>
+        <label for="lastname">Last name: </label>
+        <input type="text" name="state"  required>
         <br>
         <label for="lastname">Last name: </label>
-        <input type="text" name="lastname"  required>
-        <br>
-        <label for="email">email: </label>
-        <input type="email" name="email"  required>
-        <br>
-        <label for="password">password: </label>
-        <input type="password" name="password"  required>
+        <input type="text" name="county"  required>
         <br>
         <input type="submit" value="Login!">
     </form>
