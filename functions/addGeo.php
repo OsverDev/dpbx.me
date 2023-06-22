721fd46575ff83b2822093fde7f33fa94af56d86
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-06-22 06:35:21 -0600

    added postal code and street

--- functions/addGeo.php
+++ functions/addGeo.php
@@ -6,11 +6,14 @@
   </head>
   <body>
     <form>
-        <label for="lastname">Last name: </label>
-        <input type="text" name="state"  required>
+        <label for="lastname">Postal Code: </label>
+        <input type="text" name="postalcode"  required>
         <br>
-        <label for="lastname">Last name: </label>
+        <label for="lastname">Country: </label>
         <input type="text" name="county"  required>
+        <br>
+        <label for="lastname">text: </label>
+        <input type="text" name="street"  required>
         <br>
         <input type="submit" value="Login!">
     </form>
