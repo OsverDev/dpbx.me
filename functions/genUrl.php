cb333dae5fc3dcc109f981543be435c0f04a9619
Osver Fernandez Valverde <osver@live.com>
2023-06-22 10:05:36 -0600

    fix typos

--- functions/genUrl.php
+++ functions/genUrl.php
@@ -9,11 +9,11 @@ if (!isset($_POST['username']) and !isset($_GET['username'])) {
   //we didnt reacieve a username
   exit(0);
 }else{
-  $username = $_POST['username'].$_GET['username'];
+  $username = $_GET['username'];
 }
 
 // $table is randomString
-$key = array("username","tinyURL");
+$keys = array("username","tinyURL");
 $values = array($username,$tinyURL);
 echo insertDataIntoDatabase("tusernames", $keys, $values)? $tinyURL : '0';
 
