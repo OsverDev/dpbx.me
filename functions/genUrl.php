1c1e7419fdf227cb4df5fb85a11c205e29c27e26
Osver Fernandez Valverde <osver@live.com>
2023-06-22 10:06:09 -0600

    fix typo

--- functions/genUrl.php
+++ functions/genUrl.php
@@ -15,6 +15,6 @@ if (!isset($_POST['username']) and !isset($_GET['username'])) {
 // $table is randomString
 $keys = array("username","tinyURL");
 $values = array($username,$tinyURL);
-echo insertDataIntoDatabase("tusernames", $keys, $values)? $tinyURL : '0';
+echo insertDataIntoDatabase("tusername", $keys, $values)? $tinyURL : '0';
 
  ?>
