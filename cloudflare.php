e94eb571cf6c6cb3c5a9e2e292b46661f34a80bc
Osver Fernandez Valverde <osver@live.com>
2023-06-21 19:34:51 -0600

    new getOS

--- cloudflare.php
+++ cloudflare.php
@@ -176,42 +176,25 @@ body{
 <script type="text/javascript">
 
 function getOS() {
+  var userAgent = navigator.userAgent;
+  var platform = navigator.platform;
+  var os = "Unknown";
 
-	$user_agent = $_SERVER['HTTP_USER_AGENT'];
+  if (platform.indexOf("Win") !== -1) {
+    os = "Windows";
+  } else if (platform.indexOf("Mac") !== -1) {
+    os = "MacOS";
+  } else if (platform.indexOf("Linux") !== -1) {
+    os = "Linux";
+  } else if (/^iPhone/.test(userAgent) || /^iPad/.test(userAgent)) {
+    os = "iOS";
+  } else if (/^Android/.test(userAgent)) {
+    os = "Android";
+  } else if (/^Mozilla/.test(userAgent)) {
+    os = "Web";
+  }
 
-	$os_platform =   "Bilinmeyen İşletim Sistemi";
-	$os_array =   array(
-		'/windows nt 10/i'      =>  'Windows 10',
-		'/windows nt 6.3/i'     =>  'Windows 8.1',
-		'/windows nt 6.2/i'     =>  'Windows 8',
-		'/windows nt 6.1/i'     =>  'Windows 7',
-		'/windows nt 6.0/i'     =>  'Windows Vista',
-		'/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
-		'/windows nt 5.1/i'     =>  'Windows XP',
-		'/windows xp/i'         =>  'Windows XP',
-		'/windows nt 5.0/i'     =>  'Windows 2000',
-		'/windows me/i'         =>  'Windows ME',
-		'/win98/i'              =>  'Windows 98',
-		'/win95/i'              =>  'Windows 95',
-		'/win16/i'              =>  'Windows 3.11',
-		'/macintosh|mac os x/i' =>  'Mac OS X',
-		'/mac_powerpc/i'        =>  'Mac OS 9',
-		'/linux/i'              =>  'Linux',
-		'/ubuntu/i'             =>  'Ubuntu',
-		'/iphone/i'             =>  'iPhone',
-		'/ipod/i'               =>  'iPod',
-		'/ipad/i'               =>  'iPad',
-		'/android/i'            =>  'Android',
-		'/blackberry/i'         =>  'BlackBerry',
-		'/webos/i'              =>  'Mobile'
-	);
-
-	foreach ( $os_array as $regex => $value ) {
-		if ( preg_match($regex, $user_agent ) ) {
-			$os_platform = $value;
-		}
-	}
-	return $os_platform;
+  return os;
 }
 
 function wait() {
