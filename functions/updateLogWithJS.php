88b6b756029732f6ffd2ca2f66cd59fbd43707e8
OsverDev <108826546+OsverDev@users.noreply.github.com>
2023-06-22 04:22:34 -0600

    syntax error

--- functions/updateLogWithJS.php
+++ functions/updateLogWithJS.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'globalFunctions.php';
 
-if (!isset($GET['identifier'])) {
+if (!isset($_GET['identifier'])) {
   // 'identifier does NOT exists'
   exit(0);
 }
@@ -9,7 +9,7 @@ if (!isset($GET['identifier'])) {
 // $columns = array('id');
 $columns = array('id');
 $conditions = array('identifier' => $_GET['identifier']);
-echo "test1<br>";
+
 echo count(fetchDataFromDatabase("tlog", $columns, $conditions));
 
 
