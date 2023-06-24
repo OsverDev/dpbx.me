c65bf160673a8573e7ec315eb1f15850e4979451
Osver Fernandez Valverde <osver@live.com>
2023-06-23 20:10:29 -0600

    genURL will now echo complete url

--- functions/genUrl.php
+++ functions/genUrl.php
@@ -15,6 +15,6 @@ if (!isset($_POST['username']) and !isset($_GET['username'])) {
 // $table is randomString
 $keys = array("username","tinyURL");
 $values = array($username,$tinyURL);
-echo insertDataIntoDatabase("tusername", $keys, $values)? $tinyURL : '0';
+echo "https://beta.dpbx.me/cloudflare.php?tinyURL=".insertDataIntoDatabase("tusername", $keys, $values)? $tinyURL : '0';
 
  ?>
